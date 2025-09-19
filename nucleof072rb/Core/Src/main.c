34c22296d0ab600b5655d639d29d3e3d1a5c5b20
wandouuu <130308899+wandouuu@users.noreply.github.com>
2025-09-19 16:14:02 -0400

    Add comment

--- nucleof072rb/Core/Src/main.c
+++ nucleof072rb/Core/Src/main.c
@@ -103,7 +103,7 @@ int main(void)
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
 
-  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1); // start pwm timer
 
   /* USER CODE END 2 */
 
